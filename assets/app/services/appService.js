a09a8896ca79e851cfeda1cd754c52f9a2d2b69d
Francis <francis@creativedevlabs.com>
2018-04-09 22:25:25 +0800

    Added values to the destination drop-down list. Changed the One Way and Round Trip buttons.

--- assets/app/services/appService.js
+++ assets/app/services/appService.js
@@ -1,6 +1,6 @@
 
 angular.module('BookingApplication').factory('appService',['$http',function ($http){
-    var baseUrl = 'http://localhost/api/';
+    var baseUrl = 'http://192.168.254.103/api/';
     var factory = {};
     factory.getLocation = function() {
         return $http.get(baseUrl + 'location');
