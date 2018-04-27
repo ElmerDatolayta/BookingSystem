72a9c9f99775459b0d820561f42822e6058b9c79
Francis Gelsano <fgelsano.virus@gmail.com>
2018-04-27 15:29:10 +0800

    Fixed the booking box.

--- assets/app/services/appService.js
+++ assets/app/services/appService.js
@@ -1,6 +1,6 @@
 
 angular.module('BookingApplication').factory('appService',['$http',function ($http){
-    var baseUrl = 'http://192.168.254.103/api/';
+    var baseUrl = 'http://192.168.254.105/api/';
     var factory = {};
     factory.getLocation = function() {
         return $http.get(baseUrl + 'location');
